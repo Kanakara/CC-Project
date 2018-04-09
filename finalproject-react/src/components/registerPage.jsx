abbf8d41ba3729aafebc86419b997181f246775e
Luis Lopez <delucholuis@gmail.com>
2018-04-09 14:27:56 -0700

    Revert "json files and gitignore"
    This reverts commit 8c2b979eadde8f071cfec96b8f72c82a66171316.

---
+++ finalproject-react/src/components/registerPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import RegisterForm from './registerForm';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userRegisterRequest } from '../actions/registerActions'; 
+
+
+class RegisterPage extends React.Component {
+    render() {
+        const { userRegisterRequest } = this.props;
+        return (
+            <div className="row">
+                <div className="col-md-4 col-md-offset-4">
+                    <RegisterForm userRegisterRequest={userRegisterRequest}/>
+                </div>
+            </div>
+
+        );
+    }
+
+}
+// Commented out until i can get all of it to work
+// RegisterPage.propTypes = {
+//     userRegisterRequest: React.PropTypes.func.isRequired
+// }
+
+export default connect(null, { userRegisterRequest }) (RegisterPage); 
---
+++ finalproject-react/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import Main from './components/main';
+import { Provider } from 'react-redux';
+
+import thunk from 'redux-thunk';
+
+import store from './helpers/store';
+import routes from './routes';
+import registerServiceWorker from './registerServiceWorker';
+
+// This is the code that is making things not work,
+// because we need to set up the reducers for the project
+
+{/* // import { ConnectedRouter as Router, routerReducer, routerMiddleware } from 'react-router-redux';
+// import createHistory from 'history/createBrowserHistory';
+// import { createStore, applyMiddleware, combineReducers } from 'redux'; */}
+
+// import registerReducer from './reducers/';
+
+{/* This will need to be implemented eventually:
+import { ApplicationState }  from './store'; */}
+
+// const history = createHistory();
+// const middleware = routerMiddleware(history);
+
+// const initialState = null;
+// const reducers = combineReducers({ register: registerReducer, router: routerReducer });
+
+
+ReactDOM.render(
+    // <Provider store={ store }>
+    // <Router history={ history } routes={ routes } />
+    // </Provider>
+    <Main />
+    , document.getElementById('root'));
+    
+registerServiceWorker();
