c961590b27a6e3b8697d6f8cad37b2aad271db6b
sherylchoun <sheryljudy@yahoo.com>
2018-05-01 09:48:28 -0700

    updates

--- finalproject-react/src/components/home.jsx
+++ finalproject-react/src/components/home.jsx
@@ -8,10 +8,26 @@ export default class Home extends React.Component {
             <Grid> 
                 <Jumbotron>
                     <h2>Welcome to the Sleepy You</h2>
-                    <p>This is a website to help you understand and maintain your sleep habits</p>
+                    <p>This is a website to help you understand and maintain your sleep habits.</p>
+             
                 </Jumbotron>
+                <Row className="show-grid text-center">
+           <Col xs={6} sm={4} smOffset={2} className="person-wrapper">
+                <Image src="/images/IMG_E0063_resized.jpg" circle className="profile-pic" />
+                  <h3>Sheryl</h3>
+              <p>From Seattle, Washington.</p>
+           </Col>
+        
+           <Col xs={6} sm={4} smOffset={2} className="person-wrapper">
+                <Image src="/images/IMG_E0063_resized.jpg" circle className="profile-pic"/>
+                <h3>Luis</h3>
+                <p>From Vancouver, Oregon.</p>
+            </Col>
+                
+            </Row>
             </Grid>
         </div>
+        
         );
     }
 }
--- finalproject-react/src/reducers/submitReducer.jsx
+++ finalproject-react/src/reducers/submitReducer.jsx
@@ -1,17 +1,17 @@
-import {FETCH_SUBMIT, NEW_SUBMIT} from '../actions/types';
+// import {FETCH_SUBMIT, NEW_SUBMIT} from '../actions/submitActions';
 
-const initialState = {
-    items: [],
-    item: {}
-}
+// const initialState = {
+//     items: [],
+//     item: {}
+// }
 
-export default function (state = initialState, action) {
-    switch(action.type) {
-        case FETCH_SUBMIT:
-        return {...state,
-            itemitems: action.payload
-        }
-       default:
-    return state;
-    }
-}
+// export default function (state = initialState, action) {
+//     switch(action.type) {
+//         case FETCH_SUBMIT:
+//         return {...state,
+//             itemitems: action.payload
+//         }
+//        default:
+//     return state;
+//     }
+// }
